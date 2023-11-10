727bdb7948907b122a5a55ce848a54e8b5489d87
PC <toqaomar24@gmail.com>
2023-11-11 00:28:50 +0200

    new3

--- src/main/java/com/app/MyAppApplication.java
+++ src/main/java/com/app/MyAppApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyAppApplication {
     public static void main(String[] args) {
+
+
         SpringApplication.run(MyAppApplication.class, args);
     }
 }
